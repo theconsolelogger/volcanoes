dcb3c4d9721ce66662061c36f8044b55af6d9360
Francis <Francisy@shu.edu.cn>
2018-03-24 22:16:49 +0800

    Add the function of  selecting date to show the data as  that date.

--- NYC.php
+++ NYC.php
@@ -3,8 +3,11 @@
 	header('Access-Control-Allow-Origin:*');  
 	header('Access-Control-Allow-Methods:POST');  
 	header('Access-Control-Allow-Headers:x-requested-with,content-type');
-	$json = file_get_contents('data/4.json'); 
-	echo $json;    
+    $year = $_POST["year"];
+    $month = $_POST["month"];
+    $day = $_POST["day"];
+	$date=$year.$month.$day;
+    $file='data/'.strval($date).'.json';
+    $json = file_get_contents($file); 
+    echo $json;
 ?>
-
-
--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,5 @@
 "use strict";
-//Get data and the translate into overlays which needed
-function GetData(url,overlays){
- $.get(url, function(data) {
-            //console.log(data[0]);
-            for (var i = 1; i < data.length; i++) {
-                //console.log(data);
-                var arr = [];
-                for (var j = 0, k = 0; j < data[i].length; j = j + 5, k++) {
-                    var overlay = {
-                        'type': data[i][j],
-                        'coordinates': [{
-                                'lat': data[i][j + 1],
-                                'lng': data[i][j + 2]
-                            },
-                            {
-                                'lat': data[i][j + 3],
-                                'lng': data[i][j + 4]
-                            }
-                        ]
-                    }
-                    //console.log(overlay);
-                    arr.push(overlay);
-                }
-                overlays.push(arr);
-                //console.log(overlays);
-            }
-        });
-        //console.log(overlays);
-}
-
+var timeCycle;
 // Cycles through overlayes
 function cycleOverlay(map, mapOverlays) {
     // Create the first overlay and add to map
@@ -36,7 +7,7 @@ function cycleOverlay(map, mapOverlays) {
     var overlays = [];
     var cnt = 0;
     // Changes overlay every 1.5 seconds
-    setInterval(function() {
+    timeCycle = setInterval(function () {
         map.removeOverlay(overlay);
         map.clearOverlays();
         if (cnt == 24)
@@ -52,7 +23,7 @@ function cycleOverlay(map, mapOverlays) {
         }
 
         for (var overlay in mapOverlays[overlayCount - 1]) {
-
+            console.log(mapOverlays.length);
             var points = [
                 getMapPoints(mapOverlays[overlayCount - 1][overlay].coordinates[0]),
                 getMapPoints(mapOverlays[overlayCount - 1][overlay].coordinates[1])
@@ -112,7 +83,6 @@ function createOverlay(type, coordinates) {
     } else {
         overlay.setImageURL('images/blackhole.png');
     }
-
     return overlay;
 }
 //Create Label for mentioning the time when cycling
@@ -120,51 +90,65 @@ function createLabel(cnt) {
     var time = ["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"];
     document.getElementById("time").innerHTML = "Time:  " + time[cnt] + "-" + time[cnt + 1];
     //console.log(label);
-    /*
-     var opts = {
-        position: new BMap.Point(-73.903952,40.767077),
-        offset: new BMap.Size(30, -30)
-    }
-    var winSize = $(window).height();
-    var label = new BMap.Label("Time:  " + time[cnt] + "-" + time[cnt + 1], opts);
-    label.setStyle({
-        color: "black",
-        fontSize: winSize / 16 + "px",
-        height: winSize / 10 + "px",
-        lineHeight: winSize / 10 + "px",
-        fontFamily: "微软雅黑"
-    });
-    map.addOverlay(label);
-    */
 }
 
 var map = createMap(getMapPoints({
     'lat': -73.937297,
     'lng': 40.750026
 }));
-var url='http://localhost/volcano/NYC.php';
-var overlays=[];
-GetData(url,overlays);
-cycleOverlay(map, overlays);
+var url = 'http://localhost/volcano/NYC.php';
+var overlays = [];
 
-/*
-var points = [new BMap.Point(116.3786889372559,39.90762965106183),
-                  new BMap.Point(116.38632786853032,39.90795884517671),
-                  new BMap.Point(116.39534009082035,39.907432133833574),
-                  new BMap.Point(116.40624058825688,39.90789300648029),
-                  new BMap.Point(116.41413701159672,39.90795884517671)
-    ];
-    //坐标转换完之后的回调函数
-    translateCallback = function (data){
-      if(data.status === 0) {
-        for (var i = 0; i < data.points.length; i++) {
-            bm.addOverlay(new BMap.Marker(data.points[i]));
-            bm.setCenter(data.points[i]);
+function getText() {
+    var getY = $("#year option:selected"); //获取选中的项
+    var getM = $("#month option:selected"); //获取选中的项
+    var getD = $("#day option:selected"); //获取选中的项
+    var year = getY.val(); //拿到选中项的值
+    var month = getM.val();
+    if (getM.val() < 10)
+        month = "0" + month;
+    var day = getD.val();
+    if (getD.val() < 10)
+        day = "0" + day;
+    //console.log(year, month, day);
+    //sent data to php server and get the data which you choose
+    $.post(url, {
+        "year": year,
+        "month": month,
+        "day": day
+    },function(data,status){
+        //console.log(data[0]);
+        console.log(status);
+        if (status=="success") {
+            for (var i = 1; i < data.length; i++) {
+            //console.log(data);
+            var arr = [];
+            for (var j = 0, k = 0; j < data[i].length; j = j + 5, k++) {
+                var overlay = {
+                    'type': data[i][j],
+                    'coordinates': [{
+                            'lat': data[i][j + 1],
+                            'lng': data[i][j + 2]
+                            },
+                        {
+                            'lat': data[i][j + 3],
+                            'lng': data[i][j + 4]
+                            }
+                        ]
+                };
+                //console.log(overlay);
+                arr.push(overlay);
+            }
+            overlays.push(arr);
+            //console.log(overlays);
+            clearInterval(timeCycle);
+            cycleOverlay(map, overlays);
+            }
         }
-      }
-    }
-    setTimeout(function(){
-        var convertor = new BMap.Convertor();
-        convertor.translate(points, 1, 5, translateCallback)
-    }, 1000);
-*/
+        else{
+            alert("Please try to select another date");
+        }
+        
+        
+    });
+}
